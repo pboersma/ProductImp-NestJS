d141e46d683d5bee441e20819a9e895717637fac
Peter Boersma <peter@growingminds.nl>
2023-08-08 18:44:48 +0200

    Created a product-mapping controller

--- src/modules/product-mapping/controllers/product-mapping.controller.ts
+++ src/modules/product-mapping/controllers/product-mapping.controller.ts
@@ -1,15 +1,49 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { InsertResult, UpdateResult } from 'typeorm';
 import { ProductMappingService } from '../services/product-mapping.service';
+import { CreateProductMappingDto } from '../dtos/create-product-mapping.dto';
+import { UpdateProductMappingDto } from '../dtos/update-product-mapping.dto';
 
 @ApiTags('product-mapping')
 @Controller('product-mapping')
 export class ProductMappingController {
   constructor(private readonly productMappingService: ProductMappingService) {}
 
+  /**
+   * Create a new product mapping.
+   *
+   * @param {CreateProductMappingDto} createDto - The create data for the product mapping.
+   *
+   * @returns {Promise<InsertResult>} The result of the create operation.
+   */
   @Post()
-  async createProductMapping(@Body() createDto: any): Promise<any> {
+  @UsePipes(ValidationPipe)
+  async createProductMapping(
+    @Body() createDto: CreateProductMappingDto,
+  ): Promise<InsertResult> {
     return this.productMappingService.create(createDto);
-    //   return this.productService.create(createDto);
+  }
+
+  /**
+   * Update an existing product mapping.
+   *
+   * @param {UpdateProductMappingDto} updateDto - The update data for the product mapping.
+   *
+   * @returns {Promise<UpdateResult>} The result of the update operation.
+   */
+  @Patch()
+  @UsePipes(ValidationPipe)
+  async updateProductMapping(
+    @Body() updateDto: UpdateProductMappingDto,
+  ): Promise<UpdateResult> {
+    return this.productMappingService.update(updateDto);
   }
 }
